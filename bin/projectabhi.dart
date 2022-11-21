f4afcb08e0ab14c54e07f990adb4e5d782c60d10
Abhi <abhiabhinav71999@gmail.com>
2022-11-21 15:14:34 +0530

    commit

---
+++ bin/projectabhi.dart
@@ -0,0 +1,16 @@
+void main(){
+  String name = "Abhi";
+  int    age  = 23;
+  int  phone  = 8281705407;
+  double mark = 8.3;
+  String email = "abhiabhinav71999@gmail.com";
+
+
+  print("-----MY Resume-----");
+  print("my name is $name");  //string interpolation = ${variablename}  my name is Abhi
+  print("i am $age yes old");
+  print("my phone number is $phone");
+  print("i have $mark cgpa in my graguation");
+  print("my email id is $email");
+
+}
